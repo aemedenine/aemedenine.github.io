e33e3db0864da473fae78189ec57135f50a66d8d
Atelier Electronique Medenine <aemedenine@gmail.com>
2026-03-11 16:36:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -94,6 +94,46 @@ loginPopup.style.display = "none";
 userBox.style.display = "flex";
 
 }
+/* ================= PARTICLES ================= */
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80
+    },
+    color: {
+      value: "#38bdf8"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#38bdf8",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  }
+});
 /* ==========================================
    HERO TEXT ANIMATION
 ========================================== */
