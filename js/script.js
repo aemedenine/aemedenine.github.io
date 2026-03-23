6f1d3485b3b83a4d6f36eacf08e75cb6ad96294f
Atelier Electronique Medenine <aemedenine@gmail.com>
2026-03-23 17:14:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -651,11 +651,5 @@ document.getElementById("appareil").addEventListener("change", function() {
     });
   }
 });
-// ================= apk =================
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(() => console.log("Service Worker Registered ✅"))
-    .catch(err => console.log("SW error:", err));
-}
 
 
